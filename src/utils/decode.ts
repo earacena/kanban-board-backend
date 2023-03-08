2b31587826de6c34235367c56e78d4a4a9998974
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-08 14:50:50 -0500

    Implement decoders for runtime type checking

---
+++ src/utils/decode.ts
@@ -0,0 +1,21 @@
+import { Errors, Type, TypeOf } from 'io-ts';
+import { pipe } from 'fp-ts/lib/function';
+import { getOrElseW } from 'fp-ts/lib/Either';
+import { failure } from 'io-ts/lib/PathReporter';
+
+export const decodeWith = <ApplicationType = any, EncodeTo = ApplicationType, DecodeFrom = unknown>(
+  codec: Type<ApplicationType, EncodeTo, DecodeFrom>,
+) => (input: DecodeFrom): ApplicationType => pipe(
+    codec.decode(input),
+    getOrElseW((errors: Errors) => {
+      throw new Error(failure(errors).join('\n'));
+    }),
+  );
+
+export const decodeResponseWith = <
+  ApplicationType = any,
+  EncodeTo = ApplicationType,
+  DecodeFrom = unknown,
+  >(
+    codec: Type<ApplicationType, EncodeTo, DecodeFrom>,
+  ) => (response: DecodeFrom): TypeOf<typeof codec> => decodeWith(codec)(response);
