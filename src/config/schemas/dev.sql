128b1e03732708276b39f3f13b3f43fba0018556
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-01 20:58:16 -0400

    Add tables for columns, boards, and cards

--- src/config/schemas/dev.sql
+++ src/config/schemas/dev.sql
@@ -1,3 +1,10 @@
+-- Declare custom types
+CREATE TYPE tag AS (
+  name TEXT NOT NULL,
+  label TEXT NOT NULL,
+  color TEXT NOT NULL
+);
+
 -- Setup schema
 CREATE TABLE users (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -7,15 +14,36 @@ CREATE TABLE users (
   date_registered TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE boards (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id INTEGER NOT NULL
+  name TEXT NOT NULL,
+  label TEXT NOT NULL,
+);
+
+CREATE TABLE cards (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+  column_id INTEGER NOT NULL,
+  brief TEXT NOT NULL,
+  body TEXT,
+  color TEXT,
+  tags tag[]
+);
+
+CREATE TABLE columns (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+  board_d INTEGER NOT NULL,
+);
+
 CREATE TABLE "session" (
   sid varchar NOT NULL COLLATE "default",
   sess json NOT NULL,
   expire timestamp(6) NOT NULL
 )
 WITH (OIDS=FALSE);
-
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
-
 CREATE INDEX "IDX_session_expire" ON "session" ("expire");
-
-select * from users;
