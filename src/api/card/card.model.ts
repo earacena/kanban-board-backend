e7940b5bca060ba1968369926e419819a0ededac
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-03 22:33:21 -0400

    Implement Card model

---
+++ src/api/card/card.model.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model, Sequelize } from 'sequelize';
+import { sequelize } from '../../utils/db';
+
+class Card extends Model {}
+Card.init(
+  {
+    id: {
+      type: DataTypes.UUIDV4,
+      primaryKey: true,
+      defaultValue: Sequelize.literal('gen_random_uuid()'),
+    },
+    userId: {
+      type: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    columnId: {
+      type: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    brief: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+    },
+    color: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'card',
+  },
+);
+
+export default Card;
