44e0ec5cac098e695cc87ea0cf33598a581a9f49
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-03 22:33:52 -0400

    Implement Tag model

---
+++ src/api/tag/tag.model.ts
@@ -0,0 +1,29 @@
+import { DataTypes, Model, Sequelize } from 'sequelize';
+import { sequelize } from '../../utils/db';
+
+class Tag extends Model {}
+Tag.init(
+  {
+    id: {
+      type: DataTypes.UUIDV4,
+      primaryKey: true,
+      defaultValue: Sequelize.literal('gen_random_uuid()'),
+    },
+    label: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    color: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'tag',
+  },
+);
+
+export default Tag;
