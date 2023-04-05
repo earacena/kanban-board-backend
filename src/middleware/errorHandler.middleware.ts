db02b2c41a12cde26ab07d90ede64f0532c5f21d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-05 16:14:39 -0400

    Implement error type based error handling

--- src/middleware/errorHandler.middleware.ts
+++ src/middleware/errorHandler.middleware.ts
@@ -1,8 +1,15 @@
 import { ErrorRequestHandler } from 'express';
+import { DecodeError } from '../utils/errors';
 
 const errorHandler: ErrorRequestHandler = (err, _req, res, next) => {
-  if (err) {
-    res.status(500).json({ error: 'internal server error' }).end();
+  if (err instanceof DecodeError) {
+    res
+      .status(400)
+      .json({ error: 'invalid credentials' });
+  } else {
+    res
+      .status(500)
+      .json({ error: 'internal server error' });
   }
 
   next(err);
