7d35872e770c37c39a71e8302f1f8fa504a1fc73
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-05 16:14:58 -0400

    Change error type to DecodeError

--- src/utils/decode.ts
+++ src/utils/decode.ts
@@ -4,13 +4,14 @@ import {
 import { pipe } from 'fp-ts/lib/function';
 import { getOrElseW } from 'fp-ts/lib/Either';
 import { failure } from 'io-ts/lib/PathReporter';
+import { DecodeError } from './errors';
 
 export const decodeWith = <ApplicationType = any, EncodeTo = ApplicationType, DecodeFrom = unknown>(
   codec: Type<ApplicationType, EncodeTo, DecodeFrom>,
 ) => (input: DecodeFrom): ApplicationType => pipe(
     codec.decode(input),
     getOrElseW((errors: Errors) => {
-      throw new Error(failure(errors).join('\n'));
+      throw new DecodeError(failure(errors).join('\n'));
     }),
   );
 
