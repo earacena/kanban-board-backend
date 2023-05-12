83367540991909d75e1baa9bcaad70e8defaeef3
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-11 23:41:52 -0400

    feat: add custom error for invalid credentials

--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -1,2 +1,3 @@
 export { default as DecodeError } from './DecodeError';
 export { default as SessionError } from './SessionError';
+export { default as InvalidCredentialsError } from './InvalidCredentialsError';
