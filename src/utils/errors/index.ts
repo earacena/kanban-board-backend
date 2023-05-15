c077d5fbdf1d1af542376703280139891de98315
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-15 01:26:28 -0400

    refactor: rename InvalidCredentialsError

--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -1,3 +1,3 @@
 export { default as DecodeError } from './DecodeError';
 export { default as SessionError } from './SessionError';
-export { default as InvalidCredentialsError } from './InvalidCredentialsError';
+export { default as IncorrectPasswordError } from './IncorrectPasswordError';
