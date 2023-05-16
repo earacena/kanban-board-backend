685900101a4ffcd515df0b2e30aee5b8ed9f0063
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-15 20:48:23 -0400

    refactor: remove unused type parsers

--- src/api/login/login.types.ts
+++ src/api/login/login.types.ts
@@ -1,12 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import { z } from 'zod';
 
-export const UserDetails = z.object({
-  id: z.string(),
-  name: z.string(),
-  username: z.string(),
-});
-
 export const UserCredentials = z.object({
   username: z.string(),
   password: z.string(),
