1ef7731af42b4cbce22d538448059a447e40ccc4
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-15 20:38:25 -0400

    refactor: re-export UserNotFoundError

--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -1,3 +1,4 @@
 export { default as DecodeError } from './DecodeError';
 export { default as SessionError } from './SessionError';
 export { default as IncorrectPasswordError } from './IncorrectPasswordError';
+export { default as UserNotFoundError } from './UserNotFoundError';
