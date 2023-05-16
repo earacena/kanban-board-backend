bdaa82933507dda801ddc1f8fade529860c1da4c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-16 01:41:17 -0400

    feat: add board types

---
+++ src/api/board/board.types.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const Board = z.object({
+  id: z.string().uuid(),
+  userId: z.string().uuid(),
+  label: z.string(),
+  dateCreated: z.date(),
+});
+
+export type BoardType = z.infer<typeof Board>;
