03d5f068a08cdb50ddac88611ba00eeda97b3bd5
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-16 20:35:28 -0400

    feat(error): add new error for unauthorized action

--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -2,3 +2,4 @@ export { default as DecodeError } from './DecodeError';
 export { default as SessionError } from './SessionError';
 export { default as IncorrectPasswordError } from './IncorrectPasswordError';
 export { default as UserNotFoundError } from './UserNotFoundError';
+export { default as UnauthorizedActionError } from './UnauthorizedActionError';
