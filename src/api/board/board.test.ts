dbf5d30bb3a9c85716430dd7ce7df7c6649150f8
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-21 17:28:49 -0400

    test(board): add tests for creating boards

--- src/api/board/board.test.ts
+++ src/api/board/board.test.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import argon2 from 'argon2';
 import app from '../../app';
 import { BoardArrayType } from './board.types';
-import { BoardResponse, BoardsResponse, ErrorResponse } from '../../app.types';
+import { ApiResponse, BoardResponse, BoardsResponse, ErrorResponse } from '../../app.types';
 import User from '../user/user.model';
 import Board from './board.model';
 
@@ -73,7 +73,7 @@ describe('Board API', () => {
       .post('/login')
       .send({ username: credentials.username, password: credentials.password })
       .expect(200)
-      .end((err, res) => {
+      .end((err) => {
         if (err) { done(err); }
         done();
       });
@@ -240,13 +240,260 @@ describe('Board API', () => {
   });
 
   describe('when creating boards', () => {
-    test('returns a created board (200)', () => {});
-    test('rejects request if given userId does not exist (400)', () => {});
-    test('rejects request if userId and/or label are not supplied (400)', () => {});
-    test('rejects request if session userId and given userId do not match (401)', () => {});
-    test('rejects request if user is not logged in (401)', () => {});
+    test('returns a created board (201)', async () => {
+      const newBoard = {
+        id: uuidv4(),
+        userId,
+        label: 'new board',
+        dateCreated: new Date(),
+      };
+
+      (Board.create as jest.Mock).mockResolvedValueOnce(newBoard);
+      (User.findByPk as jest.Mock).mockResolvedValueOnce(mockUser);
+      const response = await agent
+        .post('/api/boards')
+        .send({
+          userId,
+          label: 'new board',
+        })
+        .expect(201);
+
+      const responseData = BoardResponse.parse(JSON.parse(response.text));
+      expect(responseData.success).toBe(true);
+      expect(responseData.data).toBeDefined();
+      expect(responseData.data.board).toBeDefined();
+      expect(responseData.data.board).toStrictEqual(newBoard);
+    });
+
+    test('rejects request if given userId does not exist (400)', async () => {
+      const newBoard = {
+        id: uuidv4(),
+        userId,
+        label: 'new board',
+        dateCreated: new Date(),
+      };
+
+      (Board.create as jest.Mock).mockResolvedValueOnce(newBoard);
+      (User.findByPk as jest.Mock).mockResolvedValueOnce(null);
+      const response = await agent
+        .post('/api/boards')
+        .send({
+          userId: uuidv4(),
+          label: 'new board',
+        })
+        .expect(400);
+
+      const responseData = ErrorResponse.parse(JSON.parse(response.text));
+      expect(responseData.success).toBe(false);
+      expect(responseData.errorType).toBe('base');
+      expect(responseData.errors).toBeDefined();
+      expect(responseData.errors).toStrictEqual([
+        {
+          code: 'invalid_credentials',
+          value: '',
+          path: '',
+          message: 'user does not exist',
+        },
+      ]);
+    });
+
+    test('rejects request if userId and/or label are not supplied (400)', async () => {
+      let response = await agent
+        .post('/api/boards')
+        .send({
+          userId,
+        })
+        .expect(400);
+
+      let responseData = ErrorResponse.parse(JSON.parse(response.text));
+      expect(responseData.success).toBe(false);
+      expect(responseData.errorType).toBe('zod');
+      expect(responseData.errors).toBeDefined();
+      expect(responseData.errors).toStrictEqual([
+        {
+          code: 'invalid_type',
+          message: 'Required',
+          path: ['label'],
+        },
+      ]);
+
+      response = await agent
+        .post('/api/boards')
+        .send({
+          label: 'new board',
+        })
+        .expect(400);
+
+      responseData = ErrorResponse.parse(JSON.parse(response.text));
+      expect(responseData.success).toBe(false);
+      expect(responseData.errorType).toBe('zod');
+      expect(responseData.errors).toBeDefined();
+      expect(responseData.errors).toStrictEqual([
+        {
+          code: 'invalid_type',
+          message: 'Required',
+          path: ['userId'],
+        },
+      ]);
+
+      response = await agent
+        .post('/api/boards')
+        .send()
+        .expect(400);
+
+      responseData = ErrorResponse.parse(JSON.parse(response.text));
+      expect(responseData.success).toBe(false);
+      expect(responseData.errorType).toBe('zod');
+      expect(responseData.errors).toBeDefined();
+      expect(responseData.errors).toStrictEqual([
+        {
+          code: 'invalid_type',
+          message: 'Required',
+          path: ['userId'],
+        },
+        {
+          code: 'invalid_type',
+          message: 'Required',
+          path: ['label'],
+        },
+      ]);
+    });
+
+    test('rejects request if session userId and given userId do not match (401)', async () => {
+      (User.findByPk as jest.Mock).mockResolvedValueOnce({
+        ...mockUser,
+        id: alternativeUserId,
+      });
+
+      const response = await agent
+        .post('/api/boards')
+        .send({
+          userId: alternativeUserId,
+          label: 'test label',
+        })
+        .expect(401);
+
+      const responseData = ErrorResponse.parse(JSON.parse(response.text));
+      expect(responseData.success).toBe(false);
+      expect(responseData.errorType).toBe('base');
+      expect(responseData.errors).toBeDefined();
+      expect(responseData.errors).toStrictEqual([
+        {
+          code: 'unauthorized_action',
+          value: '',
+          path: '',
+          message: 'not authorized to perform that action',
+        },
+      ]);
+    });
+
+    test('rejects request if user is not logged in (401)', async () => {
+      const response = await api
+        .post('/api/boards')
+        .send({
+          userId,
+          label: 'test label',
+        })
+        .expect(401);
+
+      const responseData = ErrorResponse.parse(JSON.parse(response.text));
+      expect(responseData.success).toBe(false);
+      expect(responseData.errorType).toBe('base');
+      expect(responseData.errors).toBeDefined();
+      expect(responseData.errors).toStrictEqual([
+        {
+          code: 'unauthorized_action',
+          value: '',
+          path: '',
+          message: 'must be logged in to perform this action',
+        },
+      ]);
+    });
   });
 
-  describe('when deleting boards', () => {});
+  describe('when deleting boards', () => {
+    test('deletes a board (204)', async () => {
+      const testBoard = boards[0];
+      if (testBoard) {
+        const response = await agent
+          .delete(`/api/boards/${testBoard?.id}`)
+          .expect(204);
+
+        const responseData = ApiResponse.parse(JSON.parse(response.text));
+        expect(responseData.success).toBe(true);
+      } else {
+        throw new Error('undefined test data');
+      }
+    });
+
+    test('rejects deletion request if there is no valid user session', async () => {
+      const testBoard = boards[0];
+      if (testBoard) {
+        const response = await api
+          .delete(`/api/boards/${testBoard?.id}`)
+          .expect(401);
+
+        const responseData = ErrorResponse.parse(JSON.parse(response.text));
+        expect(responseData.success).toBe(false);
+        expect(responseData.errorType).toBe('base');
+        expect(responseData.errors).toStrictEqual([
+          {
+            code: 'unauthorized_action',
+            value: '',
+            path: '',
+            message: 'must be logged in to perform this action',
+          },
+        ]);
+      } else {
+        throw new Error('undefined test data');
+      }
+    });
+
+    test('rejects deletion request if user did not create board', async () => {
+      const testBoard = boards[1];
+      (Board.findByPk as jest.Mock).mockResolvedValueOnce(testBoard);
+
+      if (testBoard) {
+        const response = await api
+          .delete(`/api/boards/${testBoard?.id}`)
+          .expect(401);
+
+        const responseData = ErrorResponse.parse(JSON.parse(response.text));
+        expect(responseData.success).toBe(false);
+        expect(responseData.errorType).toBe('base');
+        expect(responseData.errors).toStrictEqual([
+          {
+            code: 'unauthorized_action',
+            value: '',
+            path: '',
+            message: 'not authorized to perform that action',
+          },
+        ]);
+      } else {
+        throw new Error('undefined test data');
+      }
+    });
+
+    test('rejects deletion request if board does not exist', async () => {
+      (Board.findByPk as jest.Mock).mockResolvedValueOnce(null);
+
+      const response = await agent
+        .delete(`/api/boards/${uuidv4()}`)
+        .expect(400);
+
+      const responseData = ErrorResponse.parse(JSON.parse(response.text));
+      expect(responseData.success).toBe(false);
+      expect(responseData.errorType).toBe('base');
+      expect(responseData.errors).toStrictEqual([
+        {
+          code: 'invalid_request',
+          value: '',
+          path: '',
+          message: 'board does not exist',
+        },
+      ]);
+    });
+  });
+
   describe('when updating boards', () => {});
 });
