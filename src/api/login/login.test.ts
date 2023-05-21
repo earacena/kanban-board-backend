9e66a34c5007e1f660c5a76e326326f0c36c5fef
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-21 16:25:58 -0400

    test(login): add test for logging out

--- src/api/login/login.test.ts
+++ src/api/login/login.test.ts
@@ -6,29 +6,42 @@ import app from '../../app';
 import { ErrorResponse, UserDetailsResponse } from '../../app.types';
 
 const api = supertest(app.app);
+const agent = supertest(app.app);
 
 jest.mock('sequelize');
 jest.mock('argon2');
 jest.mock('../user/user.model');
 
 describe('Login API', () => {
-  beforeAll(() => {
-    (User.findOne as jest.Mock).mockResolvedValue({
-      id: uuidv4(),
-      name: 'Mock User 1',
-      username: 'mockuser1',
-      passwordHash: 'password_hash',
-      dateRegistered: new Date(),
-    });
+  const credentials = {
+    username: 'testUser',
+    password: 'testPassword!1',
+  };
+
+  const mockUser = {
+    id: uuidv4(),
+    name: 'Mock User 1',
+    username: 'mockuser1',
+    passwordHash: 'password_hash',
+    dateRegistered: new Date(),
+  };
+
+  beforeAll((done) => {
+    (User.findOne as jest.Mock).mockResolvedValue(mockUser);
+    (User.findByPk as jest.Mock).mockResolvedValue(mockUser);
     (argon2.verify as jest.Mock).mockResolvedValue(true);
+
+    void agent
+      .post('/login')
+      .send(credentials)
+      .expect(200)
+      .end((err, res) => {
+        if (err) done(err);
+        done();
+      });
   });
 
   describe('when receiving a login request', () => {
-    const credentials = {
-      username: 'testUser',
-      password: 'testPassword!1',
-    };
-
     test('correct credentials return user details', async () => {
       const response = await api
         .post('/login')
@@ -85,6 +98,14 @@ describe('Login API', () => {
           value: '',
         },
       ]);
+    });
+  });
+
+  describe('when receiving a logout request', () => {
+    test('session is destroyed (200)', async () => {
+      await agent
+        .post('/logout')
+        .expect(200);
     });
   });
 });
