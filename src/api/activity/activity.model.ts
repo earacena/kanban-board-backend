c8c889e98e14939be8417f004beef0bfb48e22dd
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-22 21:56:05 -0400

    feat(activity): add Activity model

---
+++ src/api/activity/activity.model.ts
@@ -0,0 +1,29 @@
+import { DataTypes, Model, Sequelize } from 'sequelize';
+import { sequelize } from '../../utils/db';
+
+class Activity extends Model {}
+Activity.init(
+  {
+    id: {
+      type: DataTypes.UUIDV4,
+      primaryKey: true,
+      defaultValue: Sequelize.literal('gen_random_uuid()'),
+    },
+    cardId: {
+      type: DataTypes.UUIDV4(),
+      allowNull: false,
+    },
+    dateCreated: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'activity',
+  },
+);
+
+export default Activity;
