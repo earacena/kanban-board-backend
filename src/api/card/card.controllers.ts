d7a5f03956f756d144ddc2c34ecf1df76fdba536
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-22 21:55:44 -0400

    feat(card): add controller stubs

---
+++ src/api/card/card.controllers.ts
@@ -0,0 +1,15 @@
+const createCardController = async () => {};
+const getCardByIdController = async () => {};
+const getCardsByColumnIdController = async () => {};
+const updateCardController = async () => {};
+const deleteCardController = async () => {};
+
+const controllers = {
+  createCardController,
+  getCardByIdController,
+  getCardsByColumnIdController,
+  updateCardController,
+  deleteCardController,
+};
+
+export default controllers;
