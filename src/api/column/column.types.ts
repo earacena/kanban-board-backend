abc9f7f5409058fa96e0d307ed0ce2c5ead03e53
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-22 20:42:26 -0400

    feat(column): add column type parsers

---
+++ src/api/column/column.types.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+export const CreateColumnPayload = z.object({
+  userId: z.string().uuid(),
+  label: z.string(),
+  boardId: z.string(),
+});
+
+export const Column = z.object({
+  id: z.string().uuid(),
+  userId: z.string().uuid(),
+  boardId: z.string().uuid(),
+  label: z.string(),
+});
+
+export const Columns = z.array(Column);
+
+export const GetColumnByIdParams = z.object({
+  columnId: z.string(),
+});
+
+export const GetColumnsByUserIdParams = z.object({
+  userId: z.string(),
+});
+
+export const DeleteColumnByIdParams = z.object({
+  columnId: z.string(),
+});
+
+export const UpdatableColumnFields = z.object({
+  label: z.string(),
+});
