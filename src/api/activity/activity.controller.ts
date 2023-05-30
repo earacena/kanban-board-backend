28727964f4f0c1ea0f4fe0fc359dd92bdecb41e3
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-05-29 20:47:58 -0400

    refactor(activity): add stubs for controllers

---
+++ src/api/activity/activity.controller.ts
@@ -0,0 +1,12 @@
+import {
+  type Request
+} from 'express';
+
+const createActivityController = (req: Request, res: Response, next: NextFunction) => {};
+
+const controllers = {
+    createActivityController,
+};
+
+export default controllers;
+
