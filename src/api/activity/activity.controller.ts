857f9d88ec0a46dcccf126a1a461d2781ce27d86
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-06-01 20:12:47 -0400

    feat(activity): add fetching activities by card id

--- src/api/activity/activity.controller.ts
+++ src/api/activity/activity.controller.ts
@@ -7,7 +7,7 @@ import { CardNotFoundError, UnauthorizedActionError } from '../../utils/errors';
 import { Card } from '../card/card.types';
 import CardModel from '../card/card.model';
 import ActivityModel from './activity.model';
-import { CreateActivityPayload, Activity } from './activity.types';
+import { CreateActivityPayload, Activity, Activities, GetActivitiesByCardIdPayload } from './activity.types';
 
 const createActivityController = async (
   req: Request,
@@ -60,8 +60,56 @@ const createActivityController = async (
   }
 };
 
+const getActivitiesByCardIdController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const isUserSessionActive = req.sessionID && req.session.user;
+    if (!isUserSessionActive) {
+      throw new UnauthorizedActionError(
+        'must be logged in to perform this action',
+      );
+    }
+
+    const { cardId } = GetActivitiesByCardIdPayload.parse(req.body);
+
+    const cardResult = Card.safeParse(
+      await CardModel.findByPk(cardId),
+    );
+
+    if (!cardResult.success) {
+      throw new CardNotFoundError('card does not exist');
+    }
+
+    const card = cardResult.data;
+
+    const isUserIdMatching = req.session.user.id === card.userId;
+    if (!isUserIdMatching) {
+      throw new UnauthorizedActionError('not authorized to perform this action');
+    }
+
+    const activities = Activities.parse(
+      await ActivityModel.findAll({ where: { cardId } }),
+    );
+
+    res
+      .status(200)
+      .json({
+        success: true,
+        data: {
+          activities,
+        },
+      });
+  } catch (err: unknown) {
+    next(err);
+  }
+};
+
 const controllers = {
   createActivityController,
+  getActivitiesByCardIdController,
 };
 
 export default controllers;
