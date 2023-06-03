603f7261c8018baccb682b089a05667fbaa3f10b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-06-02 22:21:35 -0400

    feat(activity): add route for getting activities

--- src/api/activity/activity.routes.ts
+++ src/api/activity/activity.routes.ts
@@ -5,8 +5,10 @@ const activityRouter = Router();
 
 const {
   createActivityController,
+  getActivitiesByCardIdController,
 } = activityControllers;
 
+activityRouter.get('/:cardId', getActivitiesByCardIdController);
 activityRouter.post('/', createActivityController);
 
 export default activityRouter;
