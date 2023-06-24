9090b986b763411a29259f171918e30b7b2ee8eb
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-06-24 17:08:17 -0400

    feat(activity): add static types

--- src/api/activity/activity.types.ts
+++ src/api/activity/activity.types.ts
@@ -11,6 +11,9 @@ export const Activity = z.object({
 
 export const Activities = z.array(Activity);
 
+export type ActivityType = z.infer<typeof Activity>;
+export type ActivityArrayType = z.infer<typeof Activities>;
+
 export const CreateActivityPayload = z.object({
   cardId: z.string().uuid(),
   userId: z.string().uuid(),
